7c4672fad8fdf828df46df724edd5aa6b49173c9
Kimmo Kulovesi <kk@arkku.com>
2018-06-13 13:59:55 +0300

    Add simple unit tests

---
+++ Tests/Base64URLEncodingTests.swift
@@ -0,0 +1,48 @@
+//
+//  Base64URLEncodingTests.swift
+//
+//  Copyright (c) 2017-2018 Kimmo Kulovesi
+//  https://github.com/arkku/
+//
+
+import XCTest
+import Base64URLEncoding
+
+class Base64URLEncodingTests: XCTestCase {
+
+    let testDecoded = "Lorem?ipsum"
+    let testEncoded = "TG9yZW0_aXBzdW0"
+
+    func testEncodeString() {
+        let encoded = testDecoded.base64URLEncoded()
+        XCTAssertEqual(encoded, testEncoded)
+    }
+
+    func testDecodeString() {
+        let decoded = testEncoded.base64URLDecodedString()
+        XCTAssertEqual(decoded, testDecoded)
+    }
+
+    func testEncodeEmpty() {
+        let decoded = "".base64URLEncoded()
+        XCTAssertEqual(decoded, "")
+    }
+
+    func testDecodeEmpty() {
+        let encoded = "".base64URLDecodedString()
+        XCTAssertEqual(encoded, "")
+    }
+
+    func testEncodeDecode() {
+        let testUpToLength = 1024
+        var testData = Data()
+
+        while testData.count < testUpToLength {
+            testData.append(UInt8(arc4random() & 0xFF))
+            let encoded = testData.base64URLEncodedData()
+            let decoded = encoded.base64URLDecodedData()
+            XCTAssertEqual(decoded, testData)
+        }
+    }
+
+}
